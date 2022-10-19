5e4e5d5d57aec2e9532ea8ad3317d4a9d616fd1c
Laura <moomal.abro@gmail.com>
2022-10-19 17:42:44 +0500

    change color scheme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,44 @@
 module.exports = {
   content: ["./src/views/**/*.pug", "./src/assets/js/*.js"],
   theme: {
-    extend: {},
+		extend: {
+			colors: {
+				'twilight': {
+					50: '#E8F3F7',
+					100: '#dfeff6',
+					200: '#bfe0ee',
+					300: '#9ed0e5',
+					400: '#7ec0dd',
+					500: '#5eb1d4',
+					600: '#3ea1cc',
+					700: '#2e86ab',
+					800: '#226581',
+					900: '#153f51',
+				},
+				// 'corn': {
+				// 	100: '#fffceb',
+				// 	200: '#fff6c2',
+				// 	300: '#fff099',
+				// 	400: '#ffea70',
+				// 	500: '#ffe75c',
+				// 	600: '#ffe033',
+				// 	700: '#ffda0a',
+				// 	800: '#e0bf00',
+				// 	900: '#b89c00',
+				// },
+				'vermillion': {
+					100: '#',
+					200: '#',
+					300: '#',
+					400: '#',
+					500: '#',
+					600: '#c92918',
+					700: '#',
+					800: '#',
+					900: '#',
+				},
+			},	
+		},
   },
   plugins: [require('@tailwindcss/forms'),],
 	safelist: ['flash-success', 'flash-error', 'flash-info']

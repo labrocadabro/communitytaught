8d3b89b01f2cec09d88af8c94414de54445891b1
Laura <moomal.abro@gmail.com>
2022-10-22 09:19:19 +0500

    uncomment responsive design

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,22 +2,22 @@ closeFlash = document.querySelectorAll('.flash-close');
 
 closeFlash && closeFlash.forEach(flash => flash.addEventListener('click', () => flash.parentNode.style.display = "none"));
 
-// mobileMenu = document.getElementById('mobile-menu');
+mobileMenu = document.getElementById('mobile-menu');
 
-// mobileMenu.addEventListener('click', openMenu);
+mobileMenu.addEventListener('click', openMenu);
 
-// function openMenu() {
-// 	const desktopMenu = document.getElementById('desktop-menu');
-// 	console.log(desktopMenu.classList)
-// 	const closed = desktopMenu.classList.contains('hidden');
-// 	console.log(closed)
-// 	if (closed) {
-// 		desktopMenu.classList.add('menu-opened');
-// 		desktopMenu.classList.remove('hidden');
-// 		document.querySelector('main').style.display = "none";
-// 	} else {
-// 		desktopMenu.classList.remove('menu-opened');
-// 		desktopMenu.classList.add('hidden');
-// 		document.querySelector('main').style.display = "block";
-// 	}
-// }
+function openMenu() {
+	const desktopMenu = document.getElementById('desktop-menu');
+	console.log(desktopMenu.classList)
+	const closed = desktopMenu.classList.contains('hidden');
+	console.log(closed)
+	if (closed) {
+		desktopMenu.classList.add('menu-opened');
+		desktopMenu.classList.remove('hidden');
+		document.querySelector('main').style.display = "none";
+	} else {
+		desktopMenu.classList.remove('menu-opened');
+		desktopMenu.classList.add('hidden');
+		document.querySelector('main').style.display = "block";
+	}
+}

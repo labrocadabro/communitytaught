75f1f531da0bc6d80743c28dce3f8e3f13c40ea6
Laura <moomal.abro@gmail.com>
2022-11-21 17:56:58 +0500

    add delete classes to facilitate twitch to youtube

--- src/routes/lessonRouter.js
+++ src/routes/lessonRouter.js
@@ -1,18 +1,19 @@
-import express from 'express';
+import express from "express";
 
-import * as lessons from '../controllers/lessons.js';
+import * as lessons from "../controllers/lessons.js";
 
 const router = express.Router();
 
-router.get('/add', lessons.addEditLessonForm);
-router.get('/edit/:id', lessons.addEditLessonForm);
-router.post('/add', lessons.addEditLesson);
-router.post('/edit/:id', lessons.addEditLesson);
+router.get("/add", lessons.addEditLessonForm);
+router.get("/edit/:id", lessons.addEditLessonForm);
+router.get("/delete/:id", lessons.deleteLesson);
+router.post("/add", lessons.addEditLesson);
+router.post("/edit/:id", lessons.addEditLesson);
 
-router.get('/all', lessons.allLessons);
-router.get('/:permalink', lessons.showLesson); 
+router.get("/all", lessons.allLessons);
+router.get("/:permalink", lessons.showLesson);
 
-router.put('/watched/:id', lessons.toggleWatched); 
-router.put('/checkedin/:id', lessons.toggleCheckedIn); 
+router.put("/watched/:id", lessons.toggleWatched);
+router.put("/checkedin/:id", lessons.toggleCheckedIn);
 
 export default router;

4e0cd2eaee002e620f35093cd967de7bee0d1981
Laura Abro <l.abrocadabro@gmail.com>
2022-11-29 10:34:31 +0500

    move resources view

--- src/controllers/pages.js
+++ src/controllers/pages.js
@@ -9,60 +9,63 @@ import { getHwProgress } from "../controllers/homework.js";
 import { getLessonProgress } from "../controllers/lessons.js";
 
 export const index = (req, res) => {
-	res.render("index");
+  res.render("index");
 };
 
 export const dashboard = async (req, res) => {
-	if (!req.isAuthenticated()) return notLoggedIn(req, res);
-	let lesson = [];
-	if (req.user.currentClass) {
-		let currentLesson = await Lesson.findById(req.user.currentClass);
-		currentLesson = await getLessonProgress(req.user.id, currentLesson);
-		lesson.push(currentLesson);
-	}
-	res.render('dashboard', { lesson })
+  if (!req.isAuthenticated()) return notLoggedIn(req, res);
+  let lesson = [];
+  if (req.user.currentClass) {
+    let currentLesson = await Lesson.findById(req.user.currentClass);
+    currentLesson = await getLessonProgress(req.user.id, currentLesson);
+    lesson.push(currentLesson);
+  }
+  res.render("dashboard", { lesson });
 };
 
 export const account = (req, res) => {
-	if (!req.isAuthenticated()) return notLoggedIn(req, res);
-	res.render("account");
+  if (!req.isAuthenticated()) return notLoggedIn(req, res);
+  res.render("account");
 };
 
 export const login = (req, res) => {
-	if (req.isAuthenticated()) res.redirect('/dashboard');
-	else res.render('login');	
+  if (req.isAuthenticated()) res.redirect("/dashboard");
+  else res.render("login");
 };
 
 export const register = (req, res) => {
-	if (req.isAuthenticated()) res.redirect('/dashboard');
-	else res.render('register');
+  if (req.isAuthenticated()) res.redirect("/dashboard");
+  else res.render("register");
 };
 
 export const forgot = (req, res) => {
-	if (req.isAuthenticated()) res.redirect('/dashboard');
-	else res.render('forgot');	
+  if (req.isAuthenticated()) res.redirect("/dashboard");
+  else res.render("forgot");
 };
 
 export const reset = async (req, res) => {
-	try {
-		if (!req.query.token) return res.redirect('/login');
-		const token = await Token.findOne({ token: req.query.token });
-		if (!token) {
-			req.session.flash = { type: "error", message: ["Invalid or expired link."]}
-			return res.redirect('/forgot');
-		}
-		res.render('reset', { email: token.email, token: token.token });
-	} catch (err) {
-		console.log(err);
-		req.session.flash = { type: "error", message: ["Verification error."]}
-		res.redirect('/forgot');
-	}
+  try {
+    if (!req.query.token) return res.redirect("/login");
+    const token = await Token.findOne({ token: req.query.token });
+    if (!token) {
+      req.session.flash = {
+        type: "error",
+        message: ["Invalid or expired link."],
+      };
+      return res.redirect("/forgot");
+    }
+    res.render("reset", { email: token.email, token: token.token });
+  } catch (err) {
+    console.log(err);
+    req.session.flash = { type: "error", message: ["Verification error."] };
+    res.redirect("/forgot");
+  }
 };
 
 export const resources = (req, res) => {
-	res.render('resources');	
+  res.render("resources/index");
 };
 
 export const resourcePage = (req, res) => {
-	res.render(`resources/${req.params.page}`);	
+  res.render(`resources/${req.params.page}`);
 };

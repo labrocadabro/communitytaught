9b420f81f513d842abe6f417ecb0292411bbd31b
Laura <moomal.abro@gmail.com>
2022-11-30 07:53:47 +0500

    fix mobile menu bug

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,12 +1,12 @@
 const closeFlash = document.querySelectorAll(".flash-close");
 
 closeFlash &&
-  closeFlash.forEach((flash) =>
-    flash.addEventListener(
-      "click",
-      () => (flash.parentNode.style.display = "none")
-    )
-  );
+	closeFlash.forEach((flash) =>
+		flash.addEventListener(
+			"click",
+			() => (flash.parentNode.style.display = "none")
+		)
+	);
 
 const mobileMenu = document.getElementById("mobile-menu");
 const desktopMenu = document.getElementById("desktop-menu");
@@ -15,35 +15,47 @@ const accountDropdown = document.getElementById("account-dropdown");
 const hideOnMobile = document.querySelectorAll(".no-mobile");
 
 mobileMenu.addEventListener("click", openMobileMenu);
-accountMenu.addEventListener("click", openDropdown);
-accountDropdown.addEventListener("click", (e) => e.stopPropagation());
+accountMenu && accountMenu.addEventListener("click", openDropdown);
+accountDropdown &&
+	accountDropdown.addEventListener("click", (e) => e.stopPropagation());
 
 function openMobileMenu() {
-  const closed = desktopMenu.classList.contains("hidden");
-  if (closed) {
-    desktopMenu.classList.add("menu-opened");
-    desktopMenu.classList.remove("hidden");
-    accountDropdown.classList.remove("hidden", "absolute", "border");
-    hideOnMobile.forEach((el) => el.classList.add("hidden"));
-    document.querySelector("main").style.display = "none";
-  } else {
-    desktopMenu.classList.remove("menu-opened");
-    desktopMenu.classList.add("hidden");
-    accountDropdown.classList.add("hidden", "absolute", "border");
-    hideOnMobile.forEach((el) => el.classList.remove("hidden"));
-    document.querySelector("main").style.display = "block";
-  }
+	const closed = desktopMenu.classList.contains("hidden");
+	if (closed) {
+		desktopMenu.classList.add("menu-opened");
+		desktopMenu.classList.remove("hidden");
+		if (accountDropdown) {
+			accountDropdown.classList.remove("hidden", "absolute", "border");
+			document.getElementById("account-first").classList.remove("pt-2");
+			document.getElementById("account-last").classList.remove("pb-4");
+		}
+		hideOnMobile.forEach((el) => el.classList.add("hidden"));
+		document.querySelector("main").style.display = "none";
+		document.body.classList.add("fixed");
+	} else {
+		desktopMenu.classList.remove("menu-opened");
+		desktopMenu.classList.add("hidden");
+		if (accountDropdown) {
+			accountDropdown.classList.add("hidden", "absolute", "border");
+			document.getElementById("account-first").classList.add("pt-2");
+			document.getElementById("account-last").classList.add("pb-4");
+		}
+
+		hideOnMobile.forEach((el) => el.classList.remove("hidden"));
+		document.querySelector("main").style.display = "block";
+		document.body.classList.remove("fixed");
+	}
 }
 
 function openDropdown(e) {
-  e.stopPropagation();
-  accountDropdown.classList.toggle("hidden");
-  if (!accountDropdown.classList.contains("hidden")) {
-    document.addEventListener("click", closeDropdown);
-  }
+	e.stopPropagation();
+	accountDropdown.classList.toggle("hidden");
+	if (!accountDropdown.classList.contains("hidden")) {
+		document.addEventListener("click", closeDropdown);
+	}
 }
 
 function closeDropdown(e) {
-  accountDropdown.classList.add("hidden");
-  document.removeEventListener("click", closeDropdown);
+	accountDropdown.classList.add("hidden");
+	document.removeEventListener("click", closeDropdown);
 }

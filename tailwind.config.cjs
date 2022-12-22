02f64199ea2fbe1c4e16fc15ced4de8e18ae4229
Laura <moomal.abro@gmail.com>
2022-12-22 07:36:59 +0500

    change font to poppins

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,34 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/views/**/*.pug", "./src/assets/js/*.js"],
-  theme: {
+	content: ["./src/views/**/*.pug", "./src/assets/js/*.js"],
+	theme: {
 		screens: {
-			'xs': '640px',
-      'sm': '768px',
-      'md': '896px',
-      'lg': '1024px',
-			'xl': '1280px',
-			'2xl': '1536px',
-    },
+			xs: "640px",
+			sm: "768px",
+			md: "896px",
+			lg: "1024px",
+			xl: "1280px",
+			"2xl": "1536px",
+		},
 		extend: {
 			fontFamily: {
-        'logo': ['Orienta', 'serif'],
-				'header':  ['Kufam', 'sans-serif'],
-				'body':  ['Merriweather', 'serif'],
-				'awesome': ['"Font Awesome 6 Free"']
-      },
+				logo: ["Orienta", "serif"],
+				main: ["Poppins", "sans-serif"],
+				awesome: ['"Font Awesome 6 Free"'],
+			},
 			colors: {
-				'twilight': {
-					50: '#f1f8fa',
-					100: '#E8F3F7',
-					200: '#bfe0ee',
-					300: '#9ed0e5',
-					400: '#7ec0dd',
-					500: '#5eb1d4',
-					600: '#3ea1cc',
-					700: '#2e86ab',
-					800: '#226581',
-					900: '#153f51',
+				twilight: {
+					50: "#f1f8fa",
+					100: "#E8F3F7",
+					200: "#bfe0ee",
+					300: "#9ed0e5",
+					400: "#7ec0dd",
+					500: "#5eb1d4",
+					600: "#3ea1cc",
+					700: "#2e86ab",
+					800: "#226581",
+					900: "#153f51",
 				},
 				// 'corn': {
 				// 	100: '#fffceb',
@@ -41,27 +40,26 @@ module.exports = {
 				// 	800: '#e0bf00',
 				// 	900: '#b89c00',
 				// },
-				'vermillion': {
-					100: '#',
-					200: '#',
-					300: '#',
-					400: '#',
-					500: '#',
-					600: '#c92918',
-					700: '#',
-					800: '#',
-					900: '#',
+				vermillion: {
+					100: "#",
+					200: "#",
+					300: "#",
+					400: "#",
+					500: "#",
+					600: "#c92918",
+					700: "#",
+					800: "#",
+					900: "#",
 				},
 			},
 			textUnderlineOffset: {
-        6: '6px',
-      },
+				6: "6px",
+			},
 			textDecorationThickness: {
-        3: '3px',
-      },
-
+				3: "3px",
+			},
 		},
-  },
-  plugins: [require('@tailwindcss/forms'),],
-	safelist: ['flash-success', 'flash-error', 'flash-info']
-}
+	},
+	plugins: [require("@tailwindcss/forms")],
+	safelist: ["flash-success", "flash-error", "flash-info"],
+};

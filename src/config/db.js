c3bad1660649d510520b2a81592cc1d69be0eaa6
Colton Ehrman <coltonje95@gmail.com>
2023-12-30 09:33:16 -0600

    refactor(db): Use single env MONGODB_URI for DB
    - Simplifies DB connection across app
    - Connection between local vs remote DB is abstracted

--- src/config/db.js
+++ src/config/db.js
@@ -4,20 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const getDBUri = () => {
-    const protocol = process.env.DB_PROTOCOL ?? 'mongodb+srv';
-    const user = process.env.DB_USER;
-    const pass = process.env.DB_PASS;
-    let cluster = process.env.DB_CLUSTER;
-    const dbName = process.env.DB_NAME;
-
-    // If cluster is not set to localhost (aka local db),
-    // add the existing suffix ".58qh2.mongodb.net" text to it as to not break existing deployed app.
-    // TODO: this needs to be refactored to remove the hardcoded suffix and only use the cluster variable.
-    if (!cluster.includes('localhost')) {
-        cluster += ".58qh2.mongodb.net";
-    }
-
-    return `${protocol}://${user}:${pass}@${cluster}/${dbName}?retryWrites=true&w=majority`;
+    return process.env.MONGODB_URI;
 }
 
 const connectDB = async () => {
